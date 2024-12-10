59cfb7d9cd9638030eb80ba58e47d5931c329298
Tobias Ledin <146837673+TobiasLedin@users.noreply.github.com>
2024-12-10 21:07:48 +0100

    Initial creation

---
+++ MAUI_TravelM8/MauiProgram.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MAUI_TravelM8
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    //fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    //fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+
+                    fonts.AddFont("Roboto-Black.ttf", "RobotoBlack");
+                    fonts.AddFont("Roboto-BlackItalic.ttf", "RobotoBlackItalic");
+                    fonts.AddFont("Roboto-Bold.ttf", "RobotoBold");
+                    fonts.AddFont("Roboto-BoldItalic.ttf", "RobotoBoldItalic");
+                    fonts.AddFont("Roboto-Italic.ttf", "RobotoItalic");
+                    fonts.AddFont("Roboto-Regular.ttf", "RobotoRegular");
+                    fonts.AddFont("Roboto-Light.ttf", "RobotoLight");
+                    fonts.AddFont("Roboto-LightItalic.ttf", "RobotoLightItalic");
+                    fonts.AddFont("Roboto-Medium.ttf", "RobotoMedium");
+                    fonts.AddFont("Roboto-MediumItalic.ttf", "RobotoMediumItalic");
+                    fonts.AddFont("Roboto-Thin.ttf", "RobotoThin");
+                    fonts.AddFont("Roboto-ThinItalic.ttf", "RobotoThinItalic");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
